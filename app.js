b1668d30adb74bcef32b68b67856c13dfa91a3ee
Jonas Gertz <Geertzo@live.se>
2020-08-17 15:43:57 +0200

    crud with responses

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ const pug = require('pug')
 const bodyParser = require('body-parser')
 
 const Datastore = require('nedb-promises')
+const { json } = require('body-parser')
 let datastore = Datastore.create('blog.db')
+let db2 = Datastore.create('commentdb.db')
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
@@ -21,31 +23,50 @@ app.get('/', (req, res) => {
     res.render('index.pug', {name: 'Jonas'})
 })
 
-app.post('/add', (req, res) => {
+app.post('/add', async (req, res) => {
 
-    console.log(req.body)
-    datastore.insert({ title: req.body.title, content: req.body.content })
-    res.sendStatus(200)
+    //console.log(req.body)
+    try {
+     const result = await datastore.insert({ title: req.body.title, content: req.body.content })
+     res.json({result}).status(200)
+    } catch(e) {
+      res.json({message: e}).status(400)
+    }
+  
+})
+
+app.post('/addcomment/:blogid', async (req, res) => {
+
+  try {
+    const result = await db2.insert({ blogid: req.params.blogid, comment: req.body.comment })
+    res.json({result}).status(200)
+  } catch (e) {
+    res.json({message: e}).status(400)
+  } 
+
+})
+
+app.delete('/delete/:id', async (req, res) => {
+
+    try {
+     const result = await datastore.remove({ _id: req.params.id }, {}, function (err, numRemoved) {})
+     res.json({result}).status(200)
+    } catch (e) {
+      res.json({message: e}).status(400)
+    }
+  
     
 })
 
-app.delete('/delete/:id', (req, res) => {
+app.put('/update/:id', async (req, res) => {
 
-    console.log(req.params)
-
-    datastore.remove({ _id: req.params.id }, {}, function (err, numRemoved) {
-        console.log(numRemoved)
-        res.sendStatus(200)
-    });
+  try {
+    const result = await datastore.update({_id : req.params.id}, {title : req.body.title, content : req.body.content}, {});
+    res.json({result})
+  } catch(e) {
+    res.json({message : e}).status(400)
+  }
 })
-
-app.put('/update/:id', (req, res) => {
-
-    datastore.update({_id : req.params.id}, {title : req.body.title, content : req.body.content}, {});
-    
-})
-
-
 
 //Statisk
 app.get('/pugtest', (req, res) => {
